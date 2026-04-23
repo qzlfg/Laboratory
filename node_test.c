1e691d5b0e09491b694ba3020625551ad9fa60d5
Максим <maksim-turin-2007@mail.ru>
2026-04-23 23:12:35 +0600

    Добавлена реализация односвязного списка и тесты

---
+++ node_test.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "node.c"
+
+int main()
+{
+    printf("Запуск тестов для node.c\n");
+
+    int val1 = 10;
+
+    struct Node *head = init_node(&val1);
+    assert(head != NULL);
+    assert(head->data == &val1);
+    assert(count_node(head) == 1);
+
+    int val2 = 20;
+    int val3 = 30;
+
+    head = insert_node(head, 2, &val2);
+    head = insert_node(head, 1, &val3);
+    assert(count_node(head) == 3);
+
+    struct Node *found = find_node(head, 2);
+    assert(found != NULL);
+    assert(found->data == &val1);
+
+    head = remove_node(head, 2);
+    assert(count_node(head) == 2);
+
+    head = remove_node(head, 1);
+    head = remove_node(head, 1);
+    assert(count_node(head) == 0);
+
+    printf("Все тесты node.c пройдены успешно!\n");
+    return 0;
+}
